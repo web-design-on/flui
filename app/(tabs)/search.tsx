0da940dc7b9cbecb3a5ef146969a9a7643c7515b
Natali Schers <schersnatali@gmail.com>
2026-08-30 23:55:52 -0300

    feat: enhance SearchScreen layout and styles

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,8 +1,17 @@
 import { ThemedText } from "@/components/themed-text";
-import { FluiColors } from "@/constants/theme";
+import { BorderRadius, FluiColors, Spacing } from "@/constants/theme";
+import { MaterialIcons } from "@expo/vector-icons";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import { router, useFocusEffect } from "expo-router";
 import { useCallback, useState } from "react";
-import { Keyboard, Pressable, Text, TextInput, View } from "react-native";
+import {
+  Keyboard,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function SearchScreen() {
   const [showResults, setShowResults] = useState(false);
@@ -31,18 +40,11 @@ export default function SearchScreen() {
 
   return (
     <View style={styles.container}>
-      <View
-        style={{
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "space-between",
-          gap: 16,
-        }}
-      >
+      <View style={styles.searchContainer}>
         <TextInput
           placeholder="Pesquise aqui..."
           style={styles.searchInput}
-          placeholderTextColor="#FFFFFF"
+          placeholderTextColor={FluiColors.text}
           onSubmitEditing={handleSearch}
           value={searchInput}
           onChangeText={setSearchInput}
@@ -55,66 +57,122 @@ export default function SearchScreen() {
 
       {showResults && (
         <View style={styles.searchResults}>
-          <View style={styles.searchResultItem} id="searchResults">
-            <Pressable
-              style={styles.itemTitleContainer}
-              onPress={() => router.replace("/ponto-recarga")}
-            >
+          <Pressable
+            onPress={() => router.replace("/ponto-recarga")}
+            style={styles.searchResultItem}
+          >
+            <View style={styles.itemTitleContainer}>
               <ThemedText>Flui(ndo)</ThemedText>
               <Text style={styles.sponsoredText}>Patrocinado</Text>
-            </Pressable>
-          </View>
+            </View>
+
+            <View style={styles.resultInfo}>
+              <ThemedText>
+                <MaterialIcons
+                  color={FluiColors.mutedText}
+                  name="star"
+                  size={14}
+                />
+                4,9
+              </ThemedText>
+
+              <ThemedText> - </ThemedText>
+
+              <ThemedText>
+                <MaterialCommunityIcons name="car" /> 12min
+              </ThemedText>
+            </View>
+
+            <ThemedText style={styles.chargerTitle}>
+              Tipos de carregador disponíveis:
+            </ThemedText>
+
+            <View style={styles.chargerTypes}>
+              <Text style={styles.chargerType}>CCS (2)</Text>
+              <Text style={styles.chargerType}>CA (1)</Text>
+            </View>
+          </Pressable>
         </View>
       )}
     </View>
   );
 }
 
-const styles = {
+const styles = StyleSheet.create({
   container: {
-    marginTop: 32,
-    paddingHorizontal: 16,
+    marginTop: Spacing.xl,
+    paddingHorizontal: Spacing.md,
     flex: 1,
   },
+  searchContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    gap: Spacing.md,
+  },
   searchInput: {
-    backgroundColor: "#505050",
-    borderRadius: 32,
-    color: "#fff",
-    fontSize: 16,
-    paddingHorizontal: 17,
-    paddingVertical: 8,
+    backgroundColor: FluiColors.inputText,
+    borderRadius: BorderRadius.button,
+    color: FluiColors.text,
+    fontSize: Spacing.md,
+    paddingHorizontal: Spacing.md + 1,
+    paddingVertical: Spacing.sm,
     flex: 1,
   },
   searchResults: {
-    backgroundColor: "#505050",
-    borderTopLeftRadius: 16,
-    borderTopRightRadius: 16,
-    padding: 16,
+    backgroundColor: FluiColors.inputText,
+    borderTopLeftRadius: Spacing.md,
+    borderTopRightRadius: Spacing.md,
+    padding: Spacing.md,
     marginTop: 200,
     width: "100%",
     flex: 1,
   },
   searchResultItem: {
-    backgroundColor: "#333130",
-    borderRadius: 8,
-    padding: 8,
+    backgroundColor: FluiColors.card,
+    borderRadius: BorderRadius.input,
+    padding: Spacing.sm,
   },
   itemTitleContainer: {
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
-    gap: 16,
+    gap: Spacing.md,
   },
   sponsoredText: {
     fontSize: 10,
-    color: "#FFFFFF",
+    color: FluiColors.text,
     borderWidth: 1,
     borderColor: FluiColors.primary,
-    paddingHorizontal: 8,
-    paddingVertical: 4,
-    borderRadius: 32,
+    paddingHorizontal: Spacing.sm,
+    paddingVertical: Spacing.xs,
+    borderRadius: BorderRadius.button,
   },
   cancelText: {
     fontSize: 12,
   },
-};
+  resultInfo: {
+    flexDirection: "row",
+    gap: Spacing.xs,
+  },
+  chargerTitle: {
+    borderTopWidth: 1,
+    borderColor: FluiColors.text,
+    paddingTop: Spacing.sm,
+    marginTop: Spacing.sm,
+  },
+  chargerTypes: {
+    flexDirection: "row",
+    gap: Spacing.sm,
+    marginTop: Spacing.sm,
+  },
+  chargerType: {
+    fontSize: 12,
+    color: FluiColors.text,
+    borderWidth: 1,
+    borderColor: FluiColors.text,
+    paddingHorizontal: Spacing.sm,
+    paddingVertical: Spacing.xs,
+    borderRadius: BorderRadius.button,
+  },
+});
